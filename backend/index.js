80350b0edb41b405374d2c48dc75d16ee37876ce
RaoMaheer <raomaheer812@gmail.com>
2026-05-11 20:38:06 +0500

    fix cors

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,11 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json()); // Allows us to receive JSON in req.body
 
+const cors = require('cors');
+app.use(cors({
+  origin: 'https://lfms-git-main-raomaheers-projects.vercel.app'
+}));
+
 // Simple Test Route to check DB connection
 app.use('/api/law', lawRoutes);
 
